6a72ac7729d852bf2fc44e8ed6a4c615299621a8
bbenge <benedict.benge@schenck.de>
2025-05-20 12:41:45 +0200

    Updated integration with scheduler

--- run.py
+++ run.py
@@ -4,7 +4,9 @@
 
 from app.routes import flask_app
 from db import init_db
+from app.scheduler import init_scheduler
 
 if __name__ == "__main__":
     init_db()
+    scheduler = init_scheduler()
     flask_app.run(host="0.0.0.0", port=3000)
