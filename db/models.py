2a07c12e6fc78512cc7beb9f7c54ba17a1fcb137
bbenge <benedict.benge@schenck.de>
2025-05-20 09:45:25 +0200

    Updated code from main

--- db/models.py
+++ db/models.py
@@ -1,6 +1,7 @@
 #==========================
 # db/models.py
 #==========================
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship
